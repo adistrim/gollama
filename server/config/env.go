38654d1d06658e5429a417defe18a1ca97a6484d
Aditya Raj <adistrim.dev@gmail.com>
2025-08-06 01:01:28 +0530

    updated env config

--- server/config/env.go
+++ server/config/env.go
@@ -9,6 +9,8 @@ import (
 
 type Config struct {
 	Port string
+	BaseURL string
+	GithubToken string
 }
 
 var ENV *Config
@@ -31,8 +33,21 @@ func Load() (*Config, error) {
 		port = "8080"
 	}
 	
+	baseURL := os.Getenv("BASE_URL")
+	if baseURL == "" {
+		log.Println("No BASE_URL environment variable found, using default base URL http://localhost:11434/v1")
+		baseURL = "http://localhost:11434/v1"
+	}
+	
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	if githubToken == "" {
+		log.Println("No GITHUB_TOKEN environment variable found")
+	}
+	
 	return &Config{
 		Port: port,
+		BaseURL: baseURL,
+		GithubToken: githubToken,
 	}, nil
 }
 
