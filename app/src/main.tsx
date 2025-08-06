12022154b7faccafeb3ee59367d986f8d68f7b55
Aditya Raj <adistrim.dev@gmail.com>
2025-08-07 01:28:44 +0530

    setup tanstack

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <>
-      hello
-    </>
-  </StrictMode>,
-)
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+}
